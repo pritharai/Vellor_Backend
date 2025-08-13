8fe9c6d30ef1fbeaf745b96fe9e3b4d10f8dc5e7
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-13 10:50:55 +0530

    Created Order Schema

---
+++ src/models/Order.model.js
@@ -0,0 +1,118 @@
+const { Schema, model } = require("mongoose");
+
+const orderItemSchema = new Schema({
+  variant: {
+    type: Schema.Types.ObjectId,
+    ref: "Variant",
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ["M", "L", "XL", "XXL"],
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    validate: {
+      validator: Number.isInteger,
+      message: "Quantity must be a positive integer",
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+    validate: {
+      validator: Number.isInteger,
+      message: "Price must be a positive integer",
+    },
+  },
+});
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+      validate: {
+        validator: Number.isInteger,
+        message: "Total Amount must be a positive integer",
+      },
+    },
+    status: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    paymentId: {
+      type: String,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+    razorpayOrderId: {
+      type: String,
+      required: true,
+    },
+    razorpayPaymentId: {
+      type: String,
+    },
+    razorpaySignature: {
+      type: String,
+    },
+    expectedDelivery: {
+      type: Date,
+      required: true,
+    },
+    shippingAddress: {
+      houseNumber: {
+        type: String,
+        required: true,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      colony: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+orderSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+const Order = model("Order", orderSchema);
+module.exports = Order;
